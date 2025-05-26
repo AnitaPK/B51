1c8de1baf1c9ef613484757a7d877eb2d09bf322
AnitaPK <anita.khamitkar@gmail.com>
2025-05-26 13:11:49 +0530

    started JS and Bootstrap again

---
+++ Javascript/prog.js
@@ -0,0 +1,24 @@
+function showList(){
+    // document.querySelector('#list').textContent = "BAtch 51"
+
+    document.querySelector('#list').innerHTML = `
+                                                <li>Criminal Justice</li>
+                                                <li>Khaki</li>
+    `
+}
+
+n1 = prompt("Enter your name here")
+console.log(n1);
+
+
+function myName(){
+    MyName = document.querySelector('#name1').value;console.log(MyName)
+}
+
+
+// var a 
+// var x 
+// var lecture
+
+
+// let const 
