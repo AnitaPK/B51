af717d2de38460f002ac11877a12df53a50a6ca4
AnitaPK <anita.khamitkar@gmail.com>
2025-05-27 15:10:09 +0530

    data type, bootstrap footer

--- Javascript/prog.js
+++ Javascript/prog.js
@@ -12,7 +12,7 @@ console.log(n1);
 
 
 function myName(){
-    MyName = document.querySelector('#name1').value;console.log(MyName)
+    const MyName = document.querySelector('#name1').value;console.log(MyName)
 }
 
 
@@ -22,3 +22,63 @@ function myName(){
 
 
 // let const 
+
+
+var a = 20;
+console.log(typeof(a))
+var a ="String"
+console.log(typeof(a))
+
+console.log(a);
+
+function NumA(){
+    a =30;
+    c=10
+    console.log(a);
+}
+NumA();
+console.log(typeof(NumA),"Type of of function")
+console.log(c);
+console.log(a);
+
+let b = 2011;
+console.log(b);
+
+function NumALet(){
+    b =3011;
+    console.log(b);
+}
+NumALet();
+
+// console.log(d);
+
+
+{
+    let d=40;
+    d= 60;
+}
+console.log(b);
+
+
+// hoisting
+
+
+arr =[1,2,3]
+console.log(typeof(arr))
+
+// 1. create a variable with tells your collage name, console it
+// 2. create another variable with tells your age and console it
+
+//Practice let
+// 3. Create a let variable for your favorite color and change it to another color. Print both values.
+
+// Test var Scope
+// 4. Declare a var inside an if block and see if you can print it outside the block.
+
+//Play with const
+//5. Create a const array of 3 fruits. Add another fruit to it and print the result.
+
+//6. Try reassigning the array and see what happens.
+
+
+
