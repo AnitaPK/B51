73f08bad1209eb3cffdcb0b960acea40c3d6f323
AnitaPK <anita.khamitkar@gmail.com>
2025-05-28 13:22:29 +0530

    operatorsExpressions

---
+++ Javascript/datTypeLetConstVar.js
@@ -0,0 +1,86 @@
+// 1. create a variable with tells your collage name, console it
+// 2. create another variable with tells your age and console it
+
+//Practice let
+// 3. Create a let variable for your favorite color and change it to another color. Print both values.
+
+// Test var Scope
+// 4. Declare a var inside an if block and see if you can print it outside the block.
+
+//Play with const
+//5. Create a const array of 3 fruits. Add another fruit to it and print the result.
+
+//6. Try reassigning the array and see what happens.
+
+
+// cName = "Govt Collage Of Engg SambhajiNagar"
+// console.log(cName);
+
+// age = 10;
+// console.log(age)
+
+// let color = 'red'
+// console.log(color)
+
+// color = 'Blue'
+// console.log(color)
+
+// x = 5;
+// if(x == 5){
+//     var name1= "Wisdom Sprouts"
+// }
+// console.log(name1);
+
+// const fruits = ['Banana', 'Apple', 'Mango']
+// fruits.push('Watermelon')
+
+// console.log(fruits)
+
+// // let fruits = []
+
+// // arithmetic 
+// a = 10;
+// b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// // logical 
+// // &&   ||  !
+
+// // assignment
+
+// x = 5 
+// y = 7
+// x++; //x = x+1
+// x--   //x = x-1
+// x+10    //x = x+10
+
+// x-10    //x = x-10
+
+
+
+
+// // Comparison 
+// x>0; x<10 ; 
+// x == 5
+// x === 5
+// !=
+// !==
+// <=
+// >=
+// ? :
+
+
+
+ num1 = 5
+ num2 = '5'
+ if(num1 == num2){
+    console.log("It will check only value")
+ }
+
+ if(num1===num2){
+    console.log("It will check value with datatype")
+ }
--- Javascript/prog.js
+++ Javascript/prog.js
@@ -66,19 +66,3 @@ console.log(b);
 arr =[1,2,3]
 console.log(typeof(arr))
 
-// 1. create a variable with tells your collage name, console it
-// 2. create another variable with tells your age and console it
-
-//Practice let
-// 3. Create a let variable for your favorite color and change it to another color. Print both values.
-
-// Test var Scope
-// 4. Declare a var inside an if block and see if you can print it outside the block.
-
-//Play with const
-//5. Create a const array of 3 fruits. Add another fruit to it and print the result.
-
-//6. Try reassigning the array and see what happens.
-
-
-
