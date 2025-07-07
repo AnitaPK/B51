d099979ad99ff8d13ff50697d60d5d5d63b2291e
AnitaPK <anita.khamitkar@gmail.com>
2025-07-07 13:06:07 +0530

    dom

---
+++ Javascript/interview.js
@@ -0,0 +1,73 @@
+// str1 = 'wisdom'
+// str2 = 'sprouts'
+
+// str3 = str1.concat(" ",str2)
+// console.log(str3.toUpperCase())
+
+// output = 'WISDOM SPROUTS'
+
+// str4 = 'POONAM'
+// strRev = ''
+// for(i=str4.length-1; i>=0; i--){
+//     strRev+=str4[i]
+//     console.log(str4[i])
+// }
+
+
+// console.log(strRev)
+// arr1 = [4,7,34,76,3]
+
+// for(x in objName){
+//     console.log(x) //x is key
+// }
+
+// for(x of objName){
+//     console.log(x)  // x is value
+// }
+
+// // Write a function that returns the count of vowels in a given string.
+
+
+
+// str5 = "I am always good in everthing"
+// arrSTR = str5.split(' ') = ["I", "am", "always", "good", "in", "everthing"]
+// arrSTR[index].length
+
+// vowel = 'aeiouAEIOU'
+
+
+// for(i=0)
+
+//     // Write a function to check if a string is a palindrome.
+
+// // Write a program to find the longest word in a given string.
+
+// // Write a function to remove all duplicates from an array.
+// arr2 = [2,6,7,2,4,6]
+
+// arr3 = [2,6,7,4]
+// function removeDuplicate(){
+
+// }
+
+
+// let x = 5;
+
+// {
+//     let x = 10;
+// }
+
+// var y = 20;
+// {
+//     var y = 30
+// }
+
+
+s1 = 'silent'
+s2 = 'listen'
+if(s1.length == s2.length){
+
+    console.log(s1.split('').sort().join(''))
+    ==
+    console.log(s2.split('').sort().join('')) 
+}
