6454f0a8aa5d4d9092a96357ab146c031a6dac2e
AnitaPK <anita.khamitkar@gmail.com>
2025-07-31 12:42:36 +0530

    useEffect axios

--- react/fetch-data/src/Dashboard/Dashboard.jsx
+++ react/fetch-data/src/Dashboard/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Table from 'react-bootstrap/Table';
+import Button  from 'react-bootstrap/Button'
 
 const Dashboard = () => {
   const [projects, setProjects] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  // Pagination Logic
+  const totalPages = Math.ceil(projects.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentProjects = projects.slice(startIndex, startIndex + itemsPerPage);
+
+  const handlePrev = () => {
+    if (currentPage > 1) setCurrentPage(prev => prev - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) setCurrentPage(prev => prev + 1);
+  };
 
   function fetchDate() {
     fetch("https://674e84f1635bad45618eebc1.mockapi.io/api/v1/projects")
@@ -19,6 +36,7 @@ const Dashboard = () => {
 
   return (
     <div>
+       <div className="container mt-4">
       <Table striped>
         <thead>
           <tr>
@@ -32,10 +50,15 @@ const Dashboard = () => {
           </tr>
         </thead>
         <tbody>
-          {projects.length > 0 ? (
-            projects.map((p, i)=>(
+          {/* {projects.length > 0 ? (
+            projects.map((p, i)=>( */}
+          {currentProjects.length > 0 ? (
+            currentProjects.map((p, i) => (
                 <tr keys={i}>
-              <th>{i+1}</th>
+              <th>
+                {/* {i+1} */}
+                {startIndex + i + 1}
+                </th>
               <th>{p.ProjectName}</th>
               <th>{p.Details}</th>
               <th>{p.Department}</th>
@@ -50,6 +73,21 @@ const Dashboard = () => {
           )}
         </tbody>
       </Table>
+      {/* Pagination Controls */}
+      <div className="d-flex justify-content-between mt-3">
+        <Button variant="primary" onClick={handlePrev} disabled={currentPage === 1}>
+          Previous
+        </Button>
+
+        <div className="align-self-center">
+          Page {currentPage} of {totalPages}
+        </div>
+
+        <Button variant="primary" onClick={handleNext} disabled={currentPage === totalPages}>
+          Next
+        </Button>
+      </div>
+      </div>
     </div>
   );
 };
--- react/pro-one/src/App.js
+++ react/pro-one/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button'
 import Header from './components/Header'
+import ParentComponent from './components/CallBack';
 
 function App() {
   return (
     <>
+    <ParentComponent />
      <h1 >John Doe</h1>
      <Header /> 
       <Button ></Button>
